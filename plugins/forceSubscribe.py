44eb7d868665733b0b8976e419a2cdd62a6920a4
The Vampiro Elegant <streetdemonic003@gmail.com>
2023-07-09 03:13:00 -0300

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -25,15 +25,15 @@ async def _onUnMuteRequest(client, cb):
             if cb.message.reply_to_message.from_user.id == user_id:
               await cb.message.delete()
           except UserNotParticipant:
-            await client.answer_callback_query(cb.id, text="❗ Join the mentioned 'channel' and press the 'UnMute Me' button again.", show_alert=True)
+            await client.answer_callback_query(cb.id, text="❗ Junte-se ao mencionado 'canal' e pressione o botão 'Me Desmute' botão novamente.", show_alert=True)
       else:
-        await client.answer_callback_query(cb.id, text="❗ You are muted by admins for other reasons.", show_alert=True)
+        await client.answer_callback_query(cb.id, text="❗ Você é silenciado por administradores por outros motivos.", show_alert=True)
     else:
       if not (await client.get_chat_member(chat_id, (await client.get_me()).id)).status == 'administrator':
-        await client.send_message(chat_id, f"❗ **{cb.from_user.mention} is trying to UnMute himself but i can't unmute him because i am not an admin in this chat add me as admin again.**\n__#Leaving this chat...__")
+        await client.send_message(chat_id, f"❗ **{cb.from_user.mention} está tentando Desmutar si mesmo, mas eu não posso unmute ele porque eu não sou um administrador neste bate-papo me adicione como administrador novamente.**\n__#Saindo deste bate-papo...__")
         await client.leave_chat(chat_id)
       else:
-        await client.answer_callback_query(cb.id, text="❗ Warning: Don't click the button if you can speak freely.", show_alert=True)
+        await client.answer_callback_query(cb.id, text="❗ Aviso: Não clique no botão se puder digitar livremente.", show_alert=True)
 
 
 
@@ -54,25 +54,25 @@ async def _check_member(client, message):
       except UserNotParticipant:
         try:
           sent_message = await message.reply_text(
-              " {} , you are not subscribed to my channel yet. Please join using below button and press the UnMute Me button to unmute yourself.".format(message.from_user.mention, channel, channel),
+              " {} , você ainda não está inscrito no meu canal. Por favor, junte-se usando o botão abaixo e pressione o botão Desmute me para desativar o mute.".format(message.from_user.mention, channel, channel),
               disable_web_page_preview=True,
              reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Subscribe My Channel", url=channel_url)
+                    InlineKeyboardButton("Inscrever-se no Meu Canal", url=channel_url)
                 ],
                 [
-                    InlineKeyboardButton("UnMute Me", callback_data="onUnMuteRequest")
+                    InlineKeyboardButton("Me Desmute", callback_data="onUnMuteRequest")
                 ]
             ]
         )
           )
           await client.restrict_chat_member(chat_id, user_id, ChatPermissions(can_send_messages=False))
         except ChatAdminRequired:
-          await sent_message.edit("❗ **I am not an admin here.**\n__Make me admin with ban user permission and add me again.\n#Leaving this chat...__")
+          await sent_message.edit("❗ **Eu não sou um administrador aqui.**\n__Faça-me administrador com permissão de usuário banir e me adicione novamente.\n#Saindo deste bate-papo...__")
           await client.leave_chat(chat_id)
       except ChatAdminRequired:
-        await client.send_message(chat_id, text=f"❗ **I am not an admin in [channel]({channel_url})**\n__Make me admin in the channel and add me again.\n#Leaving this chat...__")
+        await client.send_message(chat_id, text=f"❗ **Eu não sou um administrador nesse [canal]({channel_url})**\n__Faça-me administrador no canal e adicione-me novamente.\n#Saindo deste bate-papo...__")
         await client.leave_chat(chat_id)
 
 
@@ -86,17 +86,17 @@ async def config(client, message):
       input_str = input_str.replace("@", "")
       if input_str.lower() in ("off", "no", "disable"):
         sql.disapprove(chat_id)
-        await message.reply_text("❌ **Force Subscribe is Disabled Successfully.**")
+        await message.reply_text("❌ **Force Subscribe está desativado com sucesso.**")
       elif input_str.lower() in ('clear'):
-        sent_message = await message.reply_text('**Unmuting all members who are muted by me...**')
+        sent_message = await message.reply_text('**Dessilenciando todos os membros que são silenciados por mim...**')
         try:
           for chat_member in (await client.get_chat_members(message.chat.id, filter="restricted")):
             if chat_member.restricted_by.id == (await client.get_me()).id:
                 await client.unban_chat_member(chat_id, chat_member.user.id)
                 time.sleep(1)
-          await sent_message.edit('✅ **UnMuted all members who are muted by me.**')
+          await sent_message.edit('✅ **Dessilenciado todos os membros que são silenciados por mim.**')
         except ChatAdminRequired:
-          await sent_message.edit('❗ **I am not an admin in this chat.**\n__I can\'t unmute members because i am not an admin in this chat make me admin with ban user permission.__')
+          await sent_message.edit('❗ **Eu não sou um administrador neste bate-papo.**\n__Eu não posso\'desmutar membros porque eu não sou um administrador neste bate-papo me fazer administrador com permissão de usuário restringir.__')
       else:
         try:
           await client.get_chat_member(input_str, "me")
@@ -105,11 +105,11 @@ async def config(client, message):
               channel_url = await client.export_chat_invite_link(int(input_str))
           else:
               channel_url = f"https://t.me/{input_str}"
-          await message.reply_text(f"✅ **Force Subscribe is Enabled**\n__Force Subscribe is enabled, all the group members have to subscribe this [channel]({channel_url}) in order to send messages in this group.__", disable_web_page_preview=True)
+          await message.reply_text(f"✅ **Force Subscribe está habilitado**\n__Force Subscribe está habilitado, todos os membros do grupo têm que se inscrever este [canal]({channel_url}) para enviar mensagens neste grupo.__", disable_web_page_preview=True)
         except UserNotParticipant:
-          await message.reply_text(f"❗ **Not an Admin in the Channel**\n__I am not an admin in the [channel]({channel_url}). Add me as a admin in order to enable ForceSubscribe.__", disable_web_page_preview=True)
+          await message.reply_text(f"❗ **Não é um administrador no canal**\n__Eu não sou um administrador no [canal]({channel_url}). Adicione-me como administrador para ativar o ForceSubscribe.__", disable_web_page_preview=True)
         except (UsernameNotOccupied, PeerIdInvalid):
-          await message.reply_text(f"❗ **Invalid Channel Username/ID.**")
+          await message.reply_text(f"❗ **Nome de usuário/ID do canal inválido.**")
         except Exception as err:
           await message.reply_text(f"❗ **ERROR:** ```{err}```")
     else:
@@ -119,8 +119,8 @@ async def config(client, message):
             channel_url = await client.export_chat_invite_link(int(input_str))
         else:
             channel_url = f"https://t.me/{my_channel}"
-        await message.reply_text(f"✅ **Force Subscribe is enabled in this chat.**\n__For this [Channel]({channel_url})__", disable_web_page_preview=True)
+        await message.reply_text(f"✅ **Force Subscribe está ativado neste bate-papo.**\n__Para isso [Canal]({channel_url})__", disable_web_page_preview=True)
       else:
-        await message.reply_text("❌ **Force Subscribe is disabled in this chat.**")
+        await message.reply_text("❌ **Force Subscribe está desativado neste bate-papo.**")
   else:
-      await message.reply_text("❗ **Group Creator Required**\n__You have to be the group creator to do that.__")
+      await message.reply_text("❗ **Criador de grupo necessário**\n__Você tem que ser o criador do grupo para fazer isso.__")
